623ca1bb228393102e56599c5d95f085b0a141e0
AnnaRaman <anna_raman@hotmail.co.uk>
2021-02-24 18:11:38 +0000

    Added class methods to webhook handler and webhook view

--- checkout/webhook_handler.py
+++ checkout/webhook_handler.py
@@ -18,20 +18,39 @@ class StripeWH_Handler:
         Handle a generic/unknown/unexpected webhook event
         """
         return HttpResponse(
+            content=f'Unhandled webhook received: {event["type"]}',
+            status=200)
+
+    def handle_payment_intent_succeeded(self, event):
+        """
+        Handle the payment_intent.succeeded webhook from Stripe
+        """
+        return HttpResponse(
+            content=f'Webhook received: {event["type"]}',
+            status=200)
+
+    def handle_payment_intent_payment_failed(self, event):
+        """
+        Handle the payment_intent.payment_failed webhook from Stripe
+        """
+        return HttpResponse(
             content=f'Webhook received: {event["type"]}',
             status=200)
 
 # Update profile information if save_info was checked
-profile = None
-username = intent.metadata.username
-if username != 'AnonymousUser':
-    profile = UserProfile.objects.get(user__username=username)
-    if save_info:
-        profile.default_phone_number = shipping_details.phone
-        profile.default_country = shipping_details.address.country
-        profile.default_postcode = shipping_details.address.postal_code
-        profile.default_town_or_city = shipping_details.address.city
-        profile.default_street_address1 = shipping_details.address.line1
-        profile.default_street_address2 = shipping_details.address.line2
-        profile.default_county = shipping_details.address.state
-        profile.save()
+
+
+#profile = None
+#username = intent.metadata.username
+#if username != 'AnonymousUser':
+ #   profile = UserProfile.objects.get(user__username=username)
+  #  if save_info:
+   #     profile.default_phone_number = shipping_details.phone
+    #    profile.default_country = shipping_details.address.country
+     #   profile.default_postcode = shipping_details.address.postal_code
+      #  profile.default_town_or_city = shipping_details.address.city
+         
+         #profile.default_street_address2 = shipping_details.address.line2
+       # profile.default_street_address1 = shipping_details.address.line1
+        #profile.default_county = shipping_details.address.state
+        #profile.save()
