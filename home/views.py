abe177243a002c74b8a00bf5bc15ce38a21df7ed
AnnaRaman <anna_raman@hotmail.co.uk>
2021-03-08 20:58:32 +0000

    Added products view to index.html

--- home/views.py
+++ home/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render, redirect, reverse
 from django.contrib import messages
 from django.db.models import Q
 
-from .models import Product
+from products.models import Product
 # Create your views
 #  here.
 
@@ -23,6 +23,42 @@ def index(request):
 
     context = {
         'search_term': query,
+        'products': products,
+    }
+    return render(request, 'home/index.html', context)
+
+
+def all_products(request):
+    """ A view to show all products, including sorting and search queries """
+
+    products = Product.objects.all()
+    query = None
+    sort = None
+    direction = None
+
+    if request.GET:
+
+
+        if 'category' in request.GET:
+            categories = request.GET['category'].split(',')
+            products = products.filter(category__name__in=categories)
+            categories = Category.objects.filter(name__in=categories)
+
+        if 'q' in request.GET:
+            query = request.GET['q']
+            if not query:
+                messages.error(request, "You didn't search anything!")
+                return redirect(reverse('products'))
+
+            queries = Q(name__icontains=query) | Q(description__icontains=query)
+            products = products.filter(queries)
+
+
+
+    context = {
+        'products': products,
+        'search_term': query,
+        
     }
 
-    return render(request, 'home/index.html', context)
+    return render(request, 'products/products.html', context)
